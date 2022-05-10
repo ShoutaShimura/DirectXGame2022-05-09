a07df03267f4af9d9383dc653f17a431be876ba3
ShimuraShota <k021g1445@g.neec.ac.jp>
2022-05-10 13:52:23 +0900

    re:02_01

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -1,10 +1,15 @@
 ﻿#include "GameScene.h"
 #include "TextureManager.h"
 #include <cassert>
+#include"AxisIndicator.h"
+#include"PrimitiveDrawer.h"
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+	delete model_;
+	delete debagCamera_;
+}
 
 void GameScene::Initialize() {
 
@@ -12,14 +17,43 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	//3Dモデルの生成
+	model_ = Model::Create();
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//ビュートランスフォームの初期化
+	viewProjection_.Initialize();
+
+	//デバッグカメラの生成
+	debagCamera_ = new DebugCamera(1280, 720);
+
+	//軸方向表示の表示を有効にする
+	AxisIndicator::GetInstance()->SetVisible(true);
+	//軸方向表示が参照するビュープロジェクションを指定する(アドレス渡し)
+	AxisIndicator::GetInstance()->SetTargetViewProjection(&debagCamera_->GetViewProjection());
+
+
+	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	PrimitiveDrawer::GetInstance()->SetViewProjection(&debagCamera_->GetViewProjection());
+
+
+
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	debagCamera_->Update();
+}
 
 void GameScene::Draw() {
 
 	// コマンドリストの取得
 	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
 
 #pragma region 背景スプライト描画
 	// 背景スプライト描画前処理
@@ -42,6 +76,11 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	//model_->Draw(worldTransform_, viewProjection_, textureHandle_);
+	model_->Draw(worldTransform_, debagCamera_->GetViewProjection(), textureHandle_);
+
+	//ライン描画が参照するビュープロジェクションを指定する(アドレス渡し)
+	PrimitiveDrawer::GetInstance()->DrawLine3d(Vector3(0, 0, 0), Vector3(10.0f, 0, 0), Vector4(255, 0, 0, 255));
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
